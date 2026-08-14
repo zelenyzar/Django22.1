a3d699020884c00cad866dcb3bafdd4fb5d9130a
antonina p <zelenyzar@gmail.com>
2026-08-14 04:08:59 +0300

    catalog updated by adding new items, deleting and updating them; forbidden words are added as well

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,25 @@
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from .views import ProductListView, ProductDetailView
+from .views import (
+    ProductListView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+)
 from django.conf import settings
-from . import views
+
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
+    path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+
 ]
 
 if settings.DEBUG:
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,39 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView,  UpdateView, DeleteView
+from django.urls import reverse_lazy
 from catalog.models import Product
+from catalog.forms import ProductForm
+
 
 class ProductListView(ListView):
     model = Product
+    template_name = "catalog/products_list.html"
+    context_object_name = "products"
 
 
 class ProductDetailView(DetailView):
     model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:products_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:products_list")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy("catalog:products_list")
 
 
